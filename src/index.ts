4e45feaf339f9f0083d36617204bbc598a4eea46
Erik Escoffier <erik@satellitestud.io>
2020-03-27 17:22:01 +0100

    sasas
    s

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const a: number = 45
-console.log(a)
+const a: number = 45;
+interface Test {
+  b: string;
+}
+
+interface Test2 {
+  c: number;
+}
+
+const lll: Test | Test2 = {
+  b: "rr",
+  c: 42
+};
+
+const c: Test = {
+  b: "lol"
+};
+
+console.log(c);
+console.log(lll);
